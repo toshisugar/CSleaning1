ed46de7690fa87ad0ffa294734c6b5dd125d9271
ShTair <git@vsh.jp>
2018-06-18 12:57:31 +0900

    32のコメントを更新

--- 32.DrawAPP/Form1.cs
+++ 32.DrawAPP/Form1.cs
@@ -29,7 +29,7 @@ namespace _32.DrawAPP
         {
           
 
-            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;  //左辺さえ知っていれば、Shift+Alt+F10で、候補が出てくる。usingを使うか、右辺で呼び出すか。
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;  //左辺を入力すると、右辺の候補が表示される。型名さえ知っていれば、Shift+Alt+F10で、候補が出てくる。usingを使うか、ネームスペースを含めて呼び出すか。
             //円を描画する
             SolidBrush brush = new SolidBrush(Color.Purple);
             //塗りつぶすためのブラシ作成。SolidBrush型で作ることができ、インスタンス作成時にコンストラクタの引数にブラシの色を指定する。
@@ -45,22 +45,22 @@ namespace _32.DrawAPP
             e.Graphics.FillEllipse(brush, this.startPos.X, this.startPos.Y, width, height);
         }
 
-        private void MousePressed(object sender, MouseEventArgs e)  //引数「MouseEventArgs a」。
-            //このeにはとてもたくさんのマウスデータが入っている。このeにはとてもたくさんのマウスデータが入っている。位置やホイールを何回回したか等
+        private void MousePressed(object sender, MouseEventArgs e)  //引数「MouseEventArgs e」。
+            //このeにはとてもたくさんのマウスデータが入っている。位置やホイールを何回回したか等
         {
             //円の始点座標をstartPosに保存する
             this.startPos = new Point(e.X, e.Y);
             //e情報の中のX情報とY情報それぞれをPoint型のstartPos変数に代入する。
             //startPos等は構造体でり、newで呼び出す必要はないが、座標としてわかりやすいのでこのように代入している。
-            //実は「this.startPosX = e.X;」とかで代入できる。
+            //実は「this.startPos.X = e.X;」とかで代入できる。
         }
 
         private void MouseDragged(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Left)  //e.Button(マウスボタン情報)が、MouseButton.Left(離した状態)にだったら
+            if (e.Button == MouseButtons.Left)  //e.Button(マウスボタン情報)が、MouseButton.Left(左ボタンを押している状態)だったら
             {
                 //終点座標を更新する
-                this.endPos = new Point(e.X, e.Y);  //話した瞬間のe情報のX,Yを終点として記録する
+                this.endPos = new Point(e.X, e.Y);  //離した瞬間のe情報のX,Yを終点として記録する
 
                 Invalidate();
             }
